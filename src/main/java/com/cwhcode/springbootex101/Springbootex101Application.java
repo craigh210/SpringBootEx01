cebada43dcf1e8bbeb68b5d0717a5c7b00c1bba9
craigh210 <craighunt210@gmail.com>
2019-03-20 11:58:34 -0400

    Using the command line

---
+++ src/main/java/com/cwhcode/springbootex101/Springbootex101Application.java
@@ -0,0 +1,13 @@
+package com.cwhcode.springbootex101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootex101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootex101Application.class, args);
+    }
+
+}
